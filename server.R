1db87c0c296c259cebc4797cd33535280dda8420
greermp <greermp@gmail.com>
2021-07-01 19:37:31 -0400

    add color and some parameters

--- server.R
+++ server.R
@@ -10,31 +10,38 @@
 library(shiny)
 library(scales)
 library(ggthemes)
+library(RColorBrewer)
 library(tidyverse)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     # setwd("~/MSBA/MOD5/shinyex/example") 
-    invoice <- read.csv("InvoiceSummary.csv")
+    invoice <- read.csv("data/InvoiceSummary.csv")
+    # par(mar=c(3,4,2,2))
+    # display.brewer.all()
+    palette="Dark2"
+    
+    output$revPlot <- renderPlot({
+        
+
+        vehage <- invoice %>%
+            mutate(factor=case_when(
+                CAR_YEAR>=input$age ~ paste0(input$age,' or Newer'),
+                TRUE ~ paste0('Older than ',input$age)
+            )) %>% group_by(factor) %>%  summarise(AverageInvoice=mean(TOTAL_SALE_AMOUNT))
+
+        vehage %>% ggplot(aes(x=factor, y=AverageInvoice, fill=factor)) +
+            geom_col() +
+            geom_text(nudge_y = 5,aes(x=factor, y=AverageInvoice, label=paste0(round(AverageInvoice,0),'$'))) +
+            scale_y_continuous(labels = label_comma(accuracy = NULL, scale = 1,
+                                                    prefix = "$", suffix = "",
+                                                    big.mark = ",", decimal.mark = ".")) +
+            labs(x="", title="Average Revenue Per Visit") +
+            theme_tufte()+ scale_fill_brewer(palette = palette)
+    })
     
     output$distPlot <- renderPlot({
         print(input$age)
-        
-        invoice$VIN_NUMBER[invoice$VIN_NUMBER=='NULL'] = NA
-        invoice <- invoice %>% drop_na(CUST_CREATE_DATE, VIN_NUMBER)
-        invoice=invoice %>% filter(CUST_CREATE_DATE >= '2018-01-01' & CUST_CREATE_DATE <= '2018-12-31')
-        # vehage <- invoice %>% 
-        #     mutate(factor=case_when(
-        #         CAR_YEAR>=input$age ~ paste0(input$age,' or Newer'),
-        #         TRUE ~ paste0('Older than ',input$age)
-        #     )) %>% group_by(factor) %>%  summarise(AverageInvoice=mean(TOTAL_SALE_AMOUNT))
-        # 
-        # vehage %>% ggplot(aes(x=factor, y=AverageInvoice)) +
-        #     geom_col() + 
-        #     geom_text(nudge_y = 5,aes(x=factor, y=AverageInvoice, label=paste0(round(AverageInvoice,0),'$'))) +
-        #     scale_y_continuous(labels = label_comma(accuracy = NULL, scale = 1, 
-        #                                             prefix = "$", suffix = "",
-        #                                             big.mark = ",", decimal.mark = ".")) +
-        #     labs(x="", title="Older Vehicles More Profitable on Average")
+      
         
         
         vehage <- invoice %>% 
@@ -43,15 +50,20 @@ shinyServer(function(input, output) {
                 TRUE ~ paste0('Older than ',input$age)
             )) %>% filter(visit<=6)
         
-        marketingpp=38
-        discount=1.1
+        marketingpp=as.numeric(input$marketing/2)
+        discount=1+(as.numeric(input$discount)*.01)
+        marginPercent=(100-as.numeric(input$margin))*.01
+        
+        print(marketingpp)
+        print(discount)
+        print(marginPercent)
         vehage %>% group_by(factor, visit) %>% summarise(AvgVisitRev=mean(TOTAL_SALE_AMOUNT), customers=n()) %>%  arrange(desc(visit))
         
         clvAge <- vehage %>% group_by(factor, visit) %>% summarise(AvgVisitRev=mean(TOTAL_SALE_AMOUNT), customers=n()) %>% 
             mutate(Attrition=customers/lag(customers)) %>% mutate(Attrition=case_when(is.na(Attrition)~1, TRUE ~ Attrition)) %>% 
             mutate(AttrMult=Attrition*lag(Attrition)) %>% 
             mutate(AttrMult=case_when(is.na(AttrMult)~1, TRUE ~ AttrMult)) %>% 
-            rowwise() %>% mutate(num=AvgVisitRev-(AvgVisitRev*.34)-marketingpp)%>% 
+            rowwise() %>% mutate(num=AvgVisitRev-(AvgVisitRev*marginPercent)-marketingpp)%>% 
             rowwise() %>%  mutate(denominator=case_when(
                 visit==1 ~ 1,
                 visit==2 ~ discount,
@@ -65,18 +77,13 @@ shinyServer(function(input, output) {
         
         p <- clvAge %>% group_by(factor) %>% summarise(CLV=CLV) %>% unique() 
             
-        ggplot(p, aes(x=factor, y=CLV)) + geom_col() +
+        ggplot(p, aes(x=factor, y=CLV, fill=factor)) + geom_col() +
             scale_y_continuous(labels = label_comma(accuracy = NULL, scale = 1,
                                                     prefix = "$", suffix = "",
                                                     big.mark = ",", decimal.mark = ".")) +
+            geom_text(nudge_y = 5,aes(x=factor, y=CLV, label=paste0(round(CLV,0),'$'))) +
             labs(x="", title="Customer Lifetime Value Analysis", y="USD") + 
-            theme_tufte()
-        # generate bins based on input$age from ui.R
-        # x    <- faithful[, 2]
-        # bins <- seq(min(x), max(x), length.out = input$age + 1)
-        # # 
-        # # # draw the histogram with the specified number of bins
-        # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+            theme_tufte() + scale_fill_brewer(palette = palette)
 
     })
 
--- ui.R
+++ ui.R
@@ -6,6 +6,7 @@
 #
 #    http://shiny.rstudio.com/
 #
+# setwd("~/MSBA/shinyVTA")
 
 library(shiny)
 # Define UI for application that draws a histogram
@@ -21,12 +22,17 @@ shinyUI(fluidPage(
                         "Vehicle Age Segment:",
                         min = 1995,
                         max = 2020,
-                        value = 2010)
+                        value = 2010),
+            numericInput('marketing', 'Marketing per Customer/year ($)', 76, min = 1, max = 9),
+            numericInput('margin', 'Sales Margin - Parts&Labor (%)', 64, min = 1, max = 99),
+            numericInput('discount', 'Discount Rate (%)', 10, min = 1, max = 50)
+            
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            plotOutput("distPlot"),
+            plotOutput("revPlot")
         )
     )
 ))
