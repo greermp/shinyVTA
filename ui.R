23a37771d06426b98c6d5d9bf27d6f8ba223ece1
greermp <greermp@gmail.com>
2021-07-01 20:47:08 -0400

    before changing format

--- ui.R
+++ ui.R
@@ -1,21 +1,13 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 # setwd("~/MSBA/shinyVTA")
 
 library(shiny)
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+library(shinythemes)
+shinyUI(fluidPage(theme=shinytheme("cosmo"),
 
     # Application title
     titlePanel("Vehicle Age CLV"),
 
-    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             sliderInput("age",
